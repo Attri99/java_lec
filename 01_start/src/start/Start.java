7c4b54c43d390b2aec96c459174adc5072ab5939
Attri99 <ksh09201@naver.com>
2024-11-04 13:18:32 +0900

    점심시간

---
+++ 01_start/src/start/Start.java
@@ -0,0 +1,22 @@
+package start;
+
+public class Start {
+
+  public static void main(String[] args) {
+    
+    // single-line comment
+    
+    /*
+     * 
+     * multi-line comment
+     * 
+     * */
+    
+    // Console 출력 방식
+    System.out.print(1); // 출력
+    System.out.println(2); // 출력 후 줄 바꿈
+    System.out.printf("%d\n", 3); // 3출력후 줄 바꿈
+    
+  }
+
+}
