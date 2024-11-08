96b86a2520b4f1ab6f90d8a44c92fa54ddc3a2a5
Attri99 <ksh09201@naver.com>
2024-11-08 17:58:09 +0900

    1108 퇴근

---
+++ 06_class/src/pkg13_Cart/CartEx.java
@@ -0,0 +1,25 @@
+package pkg13_Cart;
+
+public class CartEx {
+
+  public static void main(String[] args) {
+    Cart cart = new Cart(5);
+    
+    cart.addProduct(null);
+    cart.addProduct(new Product("새우깡", 1000));
+    cart.addProduct(new Product("TV", 2000));
+    cart.addProduct(new Product("컵", 3000));
+    cart.addProduct(new Product("낙지", 4000));
+    cart.addProduct(new Product("카메라", 5000));
+    cart.addProduct(new Product("책상", 6000));
+
+    cart.removeProduct(0);
+    cart.removeProduct(0);
+    
+    for(int i = 0; i < cart.getProducts().length; i++) {
+      System.out.println(cart.getProducts()[i].getName());
+      System.out.println("  " + cart.getProducts()[i].getPrice());
+    }
+  }
+
+}
