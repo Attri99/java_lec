4bf58962c2e432ba63e9721c857e234ed170e0ac
Attri99 <ksh09201@naver.com>
2024-11-12 15:31:56 +0900

    5교시

--- 07_inherit/src/pkg10_interface/C_marker/Person.java
+++ 07_inherit/src/pkg10_interface/C_marker/Person.java
@@ -2,7 +2,12 @@ package pkg10_interface.C_marker;
 
 public class Person {
 
-  public void eat(Eatable food) {
-    (Food)food.eat();
+  /*
+  public void eat(Food food) { // 모든 Food 받기
+    food.eat();
+  }*/
+  
+  public void eat (Eatable food) { // 먹을 수 있는 것만 받기
+    ((Food) food).eat();
   }
 }
