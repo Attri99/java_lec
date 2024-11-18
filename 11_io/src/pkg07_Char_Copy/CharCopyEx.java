f96ac5ae34905b6a254b37642492f73173c75f91
Attri99 <ksh09201@naver.com>
2024-11-18 16:19:33 +0900

    6교시

--- 11_io/src/pkg07_Char_Copy/CharCopyEx.java
+++ 11_io/src/pkg07_Char_Copy/CharCopyEx.java
@@ -1,5 +1,6 @@
 package pkg07_Char_Copy;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
@@ -8,18 +9,39 @@ import java.io.IOException;
 
 public class CharCopyEx {
   
+//  public static void a() {
+//    File origin = new File("C:\\Program Files\\Java\\jdk-17", "LICENSE");
+//    File copy = new File("\\storage", origin.getName());
+//    
+//    try (FileReader in = new FileReader(origin); 
+//        BufferedWriter out = new BufferedWriter(new FileWriter(copy))) {
+//      
+//      char[] cbuf = new char [4];
+//      int readChar = 0;
+//            
+//      while( (readChar = in.read(cbuf)) != -1) {
+//        out.write(cbuf, 0, readChar);
+//      }
+//      
+//      //확인
+//      System.out.println(copy.getPath() + "에 복사 완료.");
+//      
+//    } catch (IOException e) {
+//      e.printStackTrace();
+//    }
+//  }
+  
   public static void a() {
     File origin = new File("C:\\Program Files\\Java\\jdk-17", "LICENSE");
     File copy = new File("\\storage", origin.getName());
     
-    try (FileReader in = new FileReader(origin); 
+    try (BufferedReader in = new BufferedReader(new FileReader(origin)); 
         BufferedWriter out = new BufferedWriter(new FileWriter(copy))) {
       
-      char[] cbuf = new char [4];
-      int readChar = 0;
+      String line = null;
             
-      while( (readChar = in.read(cbuf)) != -1) {
-        out.write(cbuf, 0, readChar);
+      while( (line = in.readLine()) != null) {
+        out.write(line + "\n");
       }
       
       //확인
